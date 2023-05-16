14c28408ac6594582927e20c92cc4b9bc59a86da
paulsenm <paulsenm@my.lanecc.edu>
2023-05-16 10:53:23 -0700

    added separate mail module

--- lanefi/module.php
+++ lanefi/module.php
@@ -18,66 +18,31 @@ function send(){
     require  BASE_DIR . '/includes/testQuestions.php';
     $json = json_decode($questions, true);
     
-    //$lines = [];
-    // foreach($_POST as $k => $answer){
-    //     echo "<p>something<p>";
-    //     //extract($question);
-    //     $q = $json[$k];
-    //     //TODO: figure out labels
-    //     $label = $q["formQuestion"];
-    //     $lines[$label] = $answer;
-    //     echo "<p>" . $q . $answer . "<p>";
-
-    // }
- 
     $questionsFile = file_get_contents( BASE_DIR . '/cache.autoQuestions.json');
     $questionsArray = json_decode( $questionsFile, true);
     $lines = [];
     foreach($_POST as $k => $answer){
-        echo "<p>Foreach on \$_POST<p>";
-        //extract($question);
-        echo "Key was: " . $k;
         $q = $questionsArray[$k];
-        // exit;
-        // //TODO: figure out labels
         $label = $q["formQuestion"];
-        echo "Label is: " . $label;
         $lines[$label] =$label . $answer;
-        echo "<p>" . $label . $answer . "<p>";
     }
     $body = implode('<br />', $lines);
     echo $lines[0];
-    $sender = $_ENV['PHP_MAILER_SENDER_ADDRESS'];
+    // $sender = $_ENV['PHP_MAILER_SENDER_ADDRESS'];
     $recipient = $_ENV['PHP_MAILER_RECIPIENT_ADDRESS'];
-    $password = $_ENV['PHP_MAILER_PASSWORD'];
+    // $password = $_ENV['PHP_MAILER_PASSWORD'];
 
-    // $subject = $_POST['CUST_INFO']." Member Network Issue";
-    // $subject = "This is a test subject";
-    // $body = "This is a test body";
-    $status = sendMail($sender, $password, $recipient, $subject, $body);
+    $status = sendMail($recipient, $subject, $body);
     print $status;
 }
 
-function mailTest(){
-    if (isset($_POST)){
-        $data = file_get_contents("php://input");
-        $params = json_decode($data, true);
-        foreach($params as $key => $value){
-            echo "Key was: " . $key . "\nAnd data was: " . $value . "\n \n";
-        }
-    }
-    else {
-        echo "Was not set";
-        echo "Post array size was: " . sizeof($_POST);
-    }
-}
+
 
 function routes(){
    return array(
         "" => __NAMESPACE__ . "\welcome",
         "/" => __NAMESPACE__ . "\welcome",
         "diagnostic" => __NAMESPACE__ . "\welcome",
-        "send" => __NAMESPACE__ . "\send",
-        "email" => __NAMESPACE__ . "\mailTest"
+        "send" => __NAMESPACE__ . "\send"
     );
 }
